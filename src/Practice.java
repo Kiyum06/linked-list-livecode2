268d7a5fffb49c034476554e62b9801838a6d3e8
Your Name <kiyum1406@gmail.com>
2026-05-25 03:25:03 -0700

    followed the livecode

--- src/Practice.java
+++ src/Practice.java
@@ -3,9 +3,16 @@ public class Practice {
         Node myNode = new Node('w', null);
         Node otherNode = new Node('x', null);
         Node coolNode = new Node('e', null);
-        
+        Node helloNode = new Node('f', null);
         myNode.next = otherNode;
         otherNode.next = coolNode;
+        coolNode.next = helloNode;
+
+        //  int numNodes = length(myNodes);
+        // int numNodes = myNode.length();
+        // System.out.print(numNodes);
+
+        Node newHead = removeAt(myNode, 0);
 
         Node current = myNode;
 
@@ -17,9 +24,50 @@ public class Practice {
         //System.out.println(myNode.value);
         //System.out.println(otherNode.value); //System.out.println(myNode.next.value);
         //System.out.println(coolNode.value); //System.out.println(myNode.next.next.value);
-        
-
-
-        
     }
+
+    // count how many nodes are in the linked list 
+    public static int length(Node head) {
+        int count = 0; 
+
+        Node current = head;
+
+        while(current != null) {
+            count++;
+            current = current.next;
+        }
+
+        return count;
+    }
+
+    // remove the node at removeIdx
+    // head is at index
+    // example w->x->e->f
+    // removeIdx 2
+    // result w->x->f
+    // Return the new head if the list
+    public static Node removeAt(Node head, int removeIdx) {
+        if(removeIdx < 0) {
+            throw new IllegalArgumentException("removeIdx must be >= 0");
+
+        }
+        if(removeIdx == 0) {
+            return head.next;
+        }
+
+        Node current = head;
+
+        for(int i = 0; i < removeIdx -1; i++) {
+            if(current == null) {
+                throw new IndexOutOfBoundsException("removeIdx must be < len(list)");
+            }
+            current = current.next;
+        }
+
+        current.next = current.next.next;
+
+        return head;
+    }
+
+
 }
